2e68c39818ecac5337cac2820d5381d88cf08b83
Alejandro Crosa <alejandro.crosa@gmail.com>
2022-01-01 20:27:17 -0800

    wi

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -8,6 +9,8 @@ import {
 } from "remix";
 import type { MetaFunction } from "remix";
 import styles from "./tailwind.css";
+import hero from "../public/images/hero.jpeg";
+import back from "../public/images/back.svg";
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
@@ -18,7 +21,6 @@ export const meta: MetaFunction = () => {
 };
 
 export function ErrorBoundary({ error }: { error: Error }) {
-  console.error(error);
   return (
     <html>
       <head>
@@ -27,8 +29,32 @@ export function ErrorBoundary({ error }: { error: Error }) {
         <Links />
       </head>
       <body>
-        {/* add the UI you want your users to see */}
-        Something broke at root!: "{error.message}"
+        <div className="max-w-3xl mx-auto">
+          <div className="py-16 px-6">
+            <Link to="/">
+              <img src={back} />
+            </Link>
+            <div className="text-center flex">
+              <div className="w-full">
+                <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+                  Oops
+                </p>
+                <p className="mt-5 mx-auto text-xl text-gray-500">
+                  {error.message}
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <img
+                src={hero}
+                className="pt-4"
+                height={"100%"}
+                width={"100%"}
+                title="Newsletter de @metacrosa"
+              />
+            </div>
+          </div>
+        </div>
         <Scripts />
       </body>
     </html>
@@ -36,7 +62,6 @@ export function ErrorBoundary({ error }: { error: Error }) {
 }
 
 export function CatchBoundary({ error }: { error: Error }) {
-  console.error(error);
   return (
     <html>
       <head>
@@ -45,8 +70,32 @@ export function CatchBoundary({ error }: { error: Error }) {
         <Links />
       </head>
       <body>
-        {/* add the UI you want your users to see */}
-        This page was not found :(
+        <div className="max-w-3xl mx-auto">
+          <div className="py-16 px-6">
+            <Link to="/">
+              <img src={back} />
+            </Link>
+            <div className="text-center flex">
+              <div className="w-full">
+                <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+                  404
+                </p>
+                <p className="mt-5 mx-auto text-xl text-gray-500">
+                  No se encontro la pagina.
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <img
+                src={hero}
+                className="pt-4"
+                height={"100%"}
+                width={"100%"}
+                title="Newsletter de @metacrosa"
+              />
+            </div>
+          </div>
+        </div>
         <Scripts />
       </body>
     </html>
