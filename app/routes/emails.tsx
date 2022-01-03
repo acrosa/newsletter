84ff9912c37b9aab5699043523b0a5b1e1d2614e
Alejandro <ale@decisionmate.app>
2022-01-02 20:50:44 -0800

    fix layout

--- app/routes/emails.tsx
+++ app/routes/emails.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router";
 
 export default function Emails() {
   return (
-    <div className="max-w-7xl mx-auto">
-      <div className="py-16 px-4">
+    <div className="max-w-3xl mx-auto">
+      <div className="py-16 px-6">
         <div className="text-center">
           <Outlet />
         </div>
